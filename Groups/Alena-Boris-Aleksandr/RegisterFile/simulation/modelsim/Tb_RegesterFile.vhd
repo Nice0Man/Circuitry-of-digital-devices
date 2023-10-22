96a889d54e50a95dfbfe2afc60a757aac795628b
weuizx <hlebeba8810@gmail.com>
2023-10-22 20:21:53 +0300

    without tester

--- Groups/Alena-Boris-Aleksandr/RegisterFile/simulation/modelsim/Tb_RegesterFile.vhd
+++ Groups/Alena-Boris-Aleksandr/RegisterFile/simulation/modelsim/Tb_RegesterFile.vhd
@@ -14,12 +14,13 @@ ARCHITECTURE behavior OF Tb_RegesterFile IS
 	constant period : time := 10 ns;
 
 	--Inputs
-   signal tb_clk : std_logic := '1'; -- Сигнал тактирования
-   signal tb_reset : std_logic := '0'; -- Сигнал сброса
+   signal tb_clk : std_logic := '1'; -- Tactation singal
+   signal tb_reset : std_logic := '0'; -- Reset signal
 	signal tb_ReadRegister : std_logic_vector(num_reg-1 downto 0) := (others => '0');
 	signal tb_WriteRegister : std_logic_vector(num_reg-1 downto 0) := (others => '0');
 	signal tb_WriteData : std_logic_vector(reg_width-1 downto 0) := (others => '0');
-	signal tb_RegWrite : std_logic := '0';
+	signal tb_EnableWrite : std_logic := '0';
+	signal tb_EnableRead : std_logic := '0';
 
 	--Outputs
 
@@ -31,7 +32,7 @@ ARCHITECTURE behavior OF Tb_RegesterFile IS
 
 BEGIN
 
---------------------------------образ тестируемого устройства----------------------------------
+--------------------------------Testing device image----------------------------------
 
 	Ul_Reg: entity work.RegisterFile (Behavioral)
 		PORT MAP (
@@ -40,7 +41,8 @@ BEGIN
 			ReadRegister => tb_ReadRegister,
 			WriteRegister => tb_WriteRegister,
 			WriteData => tb_WriteData,
-			RegWrite => tb_RegWrite,
+			EnableWrite => tb_EnableWrite,
+			EnableRead => tb_EnableRead,
 			ReadData => tb_ReadData,
 			TimerCountRegister => tb_TimerCountRegister,
 			AccumulatorRegister => tb_AccumulatorRegister,
@@ -50,14 +52,14 @@ BEGIN
 		
 -----------------------------------------------------------------------------------------------		
 
----------------------------------Настройка тактового сигнала-----------------------------------
+---------------------------------Tactation signal settings-----------------------------------
 	
 	clock_gen: process
 	begin
-		loop                --// начало беcконечного цикла 
-			tb_clk <= not tb_clk;       --// инверсия тактирования
-			wait for period/2;  --// ожидание времени
-		end loop;           --// конец беcконечного цикла
+		loop                --// 	Start an infinite loop
+			tb_clk <= not tb_clk;       --// Tactation inversion
+			wait for period/2;  --// Wait for the half of the period
+		end loop;           --// End an infinite loop
 	end process;
 		 
     --тесты
@@ -66,34 +68,56 @@ BEGIN
 
 -----------------------------------------------------------------------------------------------
 	
----------------------------------Тест чтение регистров-----------------------------------------
 		
---		for I in 0 to 31 loop
---			tb_ReadRegister <= std_logic_vector(to_unsigned(I,5));
---			wait for 25 ns;
---		end loop;
-		
------------------------------------------------------------------------------------------------
-		
----------------------------------Тест запись в регистр-----------------------------------------
+---------------------------------Write in register test-----------------------------------------
+
 		
 		tb_WriteRegister <= "01000"; -- 8
 		tb_WriteData <= x"a5a5a5a5";
+		tb_ReadRegister <= std_logic_vector(to_unsigned(6,5));
 		wait for 50 ns;
-
-
-		tb_RegWrite <= '1';
-		wait for 10 ns;
-
-		tb_RegWrite <= '0';
-		wait for 15 ns;
 		
 		tb_reset <= '1';
 		wait for 10 ns;
 		
 		tb_reset <= '0';
-		wait for 10 ns;		
+		wait for 10 ns;
 
+
+		tb_EnableWrite <= '1';
+		tb_EnableRead <= '1';
+		tb_reset <= '1';
+		wait for 10 ns;
+
+		tb_EnableWrite <= '0';
+		tb_EnableRead <= '0';
+		tb_reset <= '0';
+		wait for 15 ns;
+		
+		tb_EnableWrite <= '1';
+		tb_EnableRead <= '1';	
+		wait for 10 ns;
+		
+		tb_EnableWrite <= '0';
+		tb_EnableRead <= '0';
+		wait for 10 ns;		
+		
+		tb_WriteRegister <= "01111"; -- 15
+		tb_WriteData <= x"b4b4b4b4";
+		tb_ReadRegister <= std_logic_vector(to_unsigned(8,5));
+		
+		tb_EnableRead <= '1';
+      wait for 10 ns;
+		
+		tb_EnableRead <= '0';
+		wait for 10 ns;
+		
+		tb_EnableWrite <= '1';
+      wait for 10 ns;
+		
+		tb_EnableWrite <= '0';
+		wait for 10 ns;
+		
 -----------------------------------------------------------------------------------------------
 
 		assert false
